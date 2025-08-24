8313578eb449cf7896d63ca32975c4aae3a7b49f
Bhumika1608 <bhumikasaha1608@gmail.com>
2025-08-24 14:16:13 +0530

    Updated backend with latest changes and CSS fixes

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-
+import os
 from pathlib import Path
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -19,6 +19,10 @@ DEFAULT_FROM_EMAIL = 'noreply@localhost'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+# Add this 👇
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -56,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
